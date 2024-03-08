3800b92a91ea1a81994063da907569516c0a303d
ronlechiht <ronlechi1998ht@gmail.com>
2024-03-08 12:27:15 +0700

    Don't fetch with search,sort,pagination feature

--- src/js/modules/customer/customer.controller.js
+++ src/js/modules/customer/customer.controller.js
@@ -1,6 +1,7 @@
 import { debounce } from '../../utils/utils'
 export class CustomerController {
   constructor(customerModel, customerView) {
+    this.customers = {}
     this.customerModel = customerModel
     this.customerView = customerView
 
@@ -12,31 +13,35 @@ export class CustomerController {
     this.customerView.bindSearchOnChanged(debounce(this.handleReloadTable))
     this.customerView.bindSortOnChanged(this.handleReloadTable)
     this.customerView.bindPagination(this.handleReloadTable)
-    this.displayCustomersTable(0)
+    this.getCustomersData(0)
   }
 
-  async displayCustomersTable(pagination) {
+  async getCustomersData(pagination) {
     try {
-      const customers = await this.customerModel.getAllCustomers()
-      this.customerView.renderCustomersTable(customers, pagination)
+      this.customers = await this.customerModel.getAllCustomers()
+      this.displayCustomersTable(pagination)
     } catch (error) {
       console.log(error)
     }
   }
 
+  displayCustomersTable(pagination) {
+    this.customerView.renderCustomersTable(this.customers, pagination)
+  }
+
   handleAddCustomer = async (customer) => {
     await this.customerModel.addCustomer(customer)
-    this.displayCustomersTable(this.customerView.pagination)
+    this.getCustomersData(this.customerView.pagination)
   }
 
   handleEditCustomer = async (customer, id) => {
     await this.customerModel.editCustomer(id, customer)
-    this.displayCustomersTable(this.customerView.pagination)
+    this.getCustomersData(this.customerView.pagination)
   }
 
   handleDeleteCustomer = async (id) => {
     await this.customerModel.deleteCustomer(id)
-    this.displayCustomersTable(this.customerView.pagination)
+    this.getCustomersData(this.customerView.pagination)
   }
 
   handleReloadTable = (pagination) => this.displayCustomersTable(pagination)
